82bb32e41640654f96534d18d6f3427521996eb6
zhy <zhy@lanl.gov>
2022-05-23 14:57:12 -0600

    EOM CCSD_EP right eigenstates

--- examples/polariton_ccsd1s1_T12.py
+++ examples/polariton_ccsd1s1_T12.py
@@ -17,7 +17,8 @@ from wick.convenience import P3, P2, P1, E1, E2, EPS1, braE1, braE2, braP1, braP
 # G: single model peice of Hamiltonian
 # 
 H1 = one_e("f", ["occ", "vir"], norder=True)
-H2 = two_e("V", ["occ", "vir"], norder=True, compress=True)
+H2 = two_e("I", ["occ", "vir"], norder=True, compress=True)
+#H2 = two_e("I", ["occ", "vir"], norder=True)
 
 # 1-boson operator
 # two_p is: \sum_{01}w_{01}[b^{\dagger}_0(nm)b_1(nm)]
@@ -25,7 +26,7 @@ H2 = two_e("V", ["occ", "vir"], norder=True, compress=True)
 #  Fock matrix and matrix of normal modes are not assumed to be diagonal, even though they will usually be diagonal
 # i./e, w_{01} = 0, w_{00} is the vibrational mode (freq)
 
-Hp = two_p("w") + one_p("G")
+Hp = two_p("w") # + one_p("G")
 print('\ntwo_p(w)=', two_p("w"))
 print('\none_p(G)=', one_p("G"))
 print('\nHp=', Hp, '\n')
@@ -34,134 +35,181 @@ print('\nHp=', Hp, '\n')
 Hep = ep11("g", ["occ", "vir"], ["nm"], norder=True)
 H = H1 + H2 + Hp + Hep
 
-print('H1', H1)
-print('H2', H2)
-print('Hep', Hep)
-print('Hp', Hp)
+print('\n H1  =\n', H1)
+print('\n H2  =\n', H2)
+print('\n Hep =\n', Hep)
+print('\n Hp  =\n', Hp)
 
 #print('\nHamiltonain is\n', H)
 
-# Fermionic excitations (single and double)
-T1 = E1("t", ["occ"], ["vir"])
-T2 = E2("t2", ["occ"], ["vir"])
+get_CCSD = False
+get_EOMCCSD = True
 
-# Bosonic excitation
-#S1 = P1("s", ["nm"])
-S1 = P1("s1", ["nm"])
-S2 = P2("s2", ["nm"])
-S3 = P3("s3", ["nm"])
-print('S1=', S1,'\n')
-print('S2=', S2,'\n')
-print('S3=', S3,'\n')
+if get_CCSD:
+    # Fermionic excitations (single and double)
+    T1 = E1("T1old", ["occ"], ["vir"])
+    T2 = E2("T2old", ["occ"], ["vir"])
 
-# coupled fermion-boson excitation
-U11 = EPS1("u", ["nm"], ["occ"], ["vir"])
-T = T1 + T2 + S1 + S2 + U11
+    # Bosonic excitation
+    #S1 = P1("s", ["nm"])
+    S1 = P1("S1old", ["nm"])
+    S2 = P2("s2", ["nm"])
+    S3 = P3("s3", ["nm"])
+    print('S1=', S1,'\n')
+    print('S2=', S2,'\n')
+    print('S3=', S3,'\n')
 
-print('\n ----------- T-1 term -----------\n')
-bra = braE1("occ", "vir")
-HT = commute(H, T)
-HTT = commute(HT, T)
-HTTT = commute(commute(commute(H2, T1), T1), T1)
+    # coupled fermion-boson excitation
+    U11 = EPS1("U11old", ["nm"], ["occ"], ["vir"])
+    T = T1 + T2 + S1 + U11
+    #T = T1 + T2 + S1 + S2 + U11
 
-S = bra*(H + HT + Fraction('1/2')*HTT + Fraction('1/6')*HTTT)
-out = apply_wick(S)
-out.resolve()
-final = AExpression(Ex=out)
-print(final)
-print(final._print_einsum('T1'))
+    print('\n ----------- T-1 term -----------\n')
+    bra = braE1("occ", "vir")
+    HT = commute(H, T)
+    HTT = commute(HT, T)
+    HTTT = commute(commute(commute(H2, T1), T1), T1)
 
-print('\n ----------- T-2 term -----------\n')
-bra = braE2("occ", "vir", "occ", "vir")
-#HTTT = commute(HTT, T)
-#HTTTT = commute(HTTT, T)
-Hbar = H + HT + Fraction('1/2')*HTT
-Hbar += Fraction('1/6')*HTTT #+ Fraction('1/24')*HTTTT
+    S = bra*(H + HT + Fraction('1/2')*HTT + Fraction('1/6')*HTTT)
+    out = apply_wick(S)
+    out.resolve()
+    final = AExpression(Ex=out)
+    print(final)
+    print(final._print_einsum('T1'))
 
-#S = bra*Hbar
-#out = apply_wick(S)
-#out.resolve()
-#final = AExpression(Ex=out)
-#print(final)
+    print('\n ----------- T-2 term -----------\n')
+    bra = braE2("occ", "vir", "occ", "vir")
+    HTTT = commute(HTT, T)
+    HTTTT = commute(HTTT, T)
+    Hbar = H + HT + Fraction('1/2')*HTT
+    Hbar += Fraction('1/6')*HTTT + Fraction('1/24')*HTTTT
 
-print('\n ----------- p1 term -----------\n')
-bra = braP1('nm') 
-Hbar = H + HT + Fraction('1/2')*HTT
-Hbar += Fraction('1/6')*HTTT
 
-S = bra*Hbar
-out = apply_wick(S)
-out.resolve()
-final = AExpression(Ex=out)
-print(final)
-print(final._print_einsum())
+    S = bra*Hbar
+    out = apply_wick(S)
+    out.resolve()
+    final = AExpression(Ex=out)
+    print(final)
+    print(final._print_einsum('T2'))
 
-print('\n ----------- p1E1 term -----------\n')
-bra = braP1E1('nm','occ', 'vir') 
-Hbar = H + HT + Fraction('1/2')*HTT
-Hbar += Fraction('1/6')*HTTT
+    print('\n ----------- p1 term -----------\n')
+    bra = braP1('nm') 
+    Hbar = H + HT + Fraction('1/2')*HTT
+    Hbar += Fraction('1/6')*HTTT
 
-S = bra*Hbar
-out = apply_wick(S)
-out.resolve()
-final = AExpression(Ex=out)
-print(final)
-print(final._print_einsum())
+    S = bra*Hbar
+    out = apply_wick(S)
+    out.resolve()
+    final = AExpression(Ex=out)
+    print(final)
+    print(final._print_einsum('S1'))
 
-#======================================================================
-#   EOM_CCSD for polariton
-#======================================================================
+    print('\n ----------- p1E1 term -----------\n')
+    bra = braP1E1('nm','occ', 'vir') 
+    Hbar = H + HT + Fraction('1/2')*HTT
+    Hbar += Fraction('1/6')*HTTT
+
+    S = bra*Hbar
+    out = apply_wick(S)
+    out.resolve()
+    final = AExpression(Ex=out)
+    print(final)
+    print(final._print_einsum('U11'))
+
+
 from wick.convenience import one_e, two_e, E1, E2, braE1, E1, E2, commute
-print(' \n EOM -CCSD equations\n')
+if get_EOMCCSD:
+    #======================================================================
+    #   EOM_CCSD for polariton
+    #======================================================================
+    print(' \n EOM-CCSD equations\n')
+    
+    print('\n CCSD Hamiltonian=\n')
+    print(H)
 
-H1 = one_e("f", ["occ", "vir"], norder=True)
-H2 = two_e("I", ["occ", "vir"], norder=True)
-H = H1 + H2
+    T1 = E1("T1", ["occ"], ["vir"])
+    T2 = E2("T2", ["occ"], ["vir"])
 
-bra = braE1("occ", "vir")
-T1 = E1("t", ["occ"], ["vir"])
-T2 = E2("t", ["occ"], ["vir"])
+    S1 = P1("S1", ["nm"])
+    S2 = P2("s2", ["nm"])
+    S3 = P3("s3", ["nm"])
+    print('S1=', S1,'\n')
+    print('S2=', S2,'\n')
+    print('S3=', S3,'\n')
 
-R1 = E1("r", ["occ"], ["vir"])
-R2 = E2("r", ["occ"], ["vir"])
+    # coupled fermion-boson excitation
+    U11 = EPS1("U11", ["nm"], ["occ"], ["vir"])
 
-T = T1 + T2
-R = R1 + R2
+    RS = E1("RS", ["occ"], ["vir"])
+    RD = E2("RD", ["occ"], ["vir"])
+    R1 = P1("R1", ["nm"])
+    R11 = EPS1("R11", ["nm"], ["occ"], ["vir"])
 
-HT = commute(H, T)
-HTT = commute(HT, T)
-HTTT = commute(HTT, T)
-HTTTT = commute(HTTT, T)
-Hbar = H + HT + Fraction('1/2')*HTT
+    T = T1 + T2 + S1 + U11
+    R = RS + RD + R1 + R11
 
-S0 = Hbar
-E0 = apply_wick(S0)
-E0.resolve()
+    HT = commute(H, T)
+    HTT = commute(HT, T)
+    HTTT = commute(HTT, T)
+    HTTTT = commute(HTTT, T)
+    Hbar = H + HT + Fraction('1/2')*HTT
 
-Hbar += Fraction('1/6')*HTTT + Fraction('1/24')*HTTTT
+    S0 = Hbar
+    E0 = apply_wick(S0)
+    E0.resolve()
 
-#   EOM - sigS part
-print('\nEOM-CCSD sigmaS part\n')
+    final = AExpression(Ex=E0)
 
-S = bra*(Hbar - E0)*R
+    Hbar += Fraction('1/6')*HTTT + Fraction('1/24')*HTTTT
 
-out = apply_wick(S)
-out.resolve()
-final = AExpression(Ex=out)
-print(final)
-print(final._print_einsum())
+    # =================  EOM - sigS part =================
+    print('\nEOM-CCSD sigS part\n')
+    bra = braE1("occ", "vir")
 
-# =================  EOM - sigD part =================
-print('\nEOM-CCSD sigmaD part\n')
-bra = braE2("occ", "vir", "occ", "vir")
+    S = bra*(Hbar - E0)*R
 
-S = bra*(Hbar - E0)*R
+    out = apply_wick(S)
+    out.resolve()
+    final = AExpression(Ex=out)
+    print(final)
+    print(final._print_einsum('SigS'))
 
-out = apply_wick(S)
-out.resolve()
-final = AExpression(Ex=out)
-print(final)
-print(final._print_einsum())
+    # =================  EOM - sigD part =================
+    print('\nEOM-CCSD sigD part\n')
+    bra = braE2("occ", "vir", "occ", "vir")
 
+    S = bra*(Hbar - E0)*R
 
+    out = apply_wick(S)
+    out.resolve()
+    final = AExpression(Ex=out)
+    print(final)
+    print('\n einsum format=\n')
+    print(final._print_einsum('SigD'))
+
+    # =================  EOM - sig1 part =================
+    print('\nEOM-CCSD sig1 part\n')
+    bra = braP1('nm') 
+
+    S = bra*(Hbar - E0)*R
+
+    out = apply_wick(S)
+    out.resolve()
+    final = AExpression(Ex=out)
+    print(final)
+    print('\n einsum format=\n')
+    print(final._print_einsum('Sig1'))
+    
+    # =================  EOM - sigU1 part =================
+    print('\nEOM-CCSD sigU1 part\n')
+    bra = braP1E1('nm','occ', 'vir') 
+
+    S = bra*(Hbar - E0)*R
+
+    out = apply_wick(S)
+    out.resolve()
+    final = AExpression(Ex=out)
+    print(final)
+    print('\n einsum format=\n')
+    print(final._print_einsum('SigU1'))
 
--- examples/polariton_ccsd1s1_T12_left.py
+++ examples/polariton_ccsd1s1_T12_left.py
@@ -41,114 +41,148 @@ print('Hp', Hp)
 
 #print('\nHamiltonain is\n', H)
 
-# Fermionic excitations (single and double)
-T1 = E1("T1old", ["occ"], ["vir"])
-T2 = E2("T2old", ["occ"], ["vir"])
+get_CCSD = True
+get_EOMCCSD = True
 
-# Bosonic excitation
-#S1 = P1("s", ["nm"])
-S1 = P1("S1old", ["nm"])
-S2 = P2("s2", ["nm"])
-S3 = P3("s3", ["nm"])
-print('S1=', S1,'\n')
-print('S2=', S2,'\n')
-print('S3=', S3,'\n')
+if get_CCSD:
+    # Fermionic excitations (single and double)
+    T1 = E1("T1old", ["occ"], ["vir"])
+    T2 = E2("T2old", ["occ"], ["vir"])
 
-# coupled fermion-boson excitation
-U11 = EPS1("U11old", ["nm"], ["occ"], ["vir"])
-T = T1 + T2 + S1 + U11
+    # Bosonic excitation
+    #S1 = P1("s", ["nm"])
+    S1 = P1("S1old", ["nm"])
+    S2 = P2("s2", ["nm"])
+    S3 = P3("s3", ["nm"])
+    print('S1=', S1,'\n')
+    print('S2=', S2,'\n')
+    print('S3=', S3,'\n')
 
-print('\n ----------- T-1 term -----------\n')
-bra = braE1("occ", "vir")
-HT = commute(H, T)
-HTT = commute(HT, T)
-HTTT = commute(commute(commute(H2, T1), T1), T1)
+    # coupled fermion-boson excitation
+    U11 = EPS1("U11old", ["nm"], ["occ"], ["vir"])
+    T = T1 + T2 + S1 + U11
+    #T = T1 + T2 + S1 + S2 + U11
 
-S = bra*(H + HT + Fraction('1/2')*HTT + Fraction('1/6')*HTTT)
-out = apply_wick(S)
-out.resolve()
-final = AExpression(Ex=out)
-print(final)
-print(final._print_einsum('T1'))
+    print('\n ----------- T-1 term -----------\n')
+    bra = braE1("occ", "vir")
+    HT = commute(H, T)
+    HTT = commute(HT, T)
+    HTTT = commute(commute(commute(H2, T1), T1), T1)
 
-print('\n ----------- T-2 term -----------\n')
-bra = braE2("occ", "vir", "occ", "vir")
-HTTT = commute(HTT, T)
-HTTTT = commute(HTTT, T)
-Hbar = H + HT + Fraction('1/2')*HTT
-Hbar += Fraction('1/6')*HTTT + Fraction('1/24')*HTTTT
+    S = bra*(H + HT + Fraction('1/2')*HTT + Fraction('1/6')*HTTT)
+    out = apply_wick(S)
+    out.resolve()
+    final = AExpression(Ex=out)
+    print(final)
+    print(final._print_einsum('T1'))
 
-
-S = bra*Hbar
-out = apply_wick(S)
-out.resolve()
-final = AExpression(Ex=out)
-print(final)
-print(final._print_einsum('T2'))
-
-print('\n ----------- p1 term -----------\n')
-bra = braP1('nm') 
-Hbar = H + HT + Fraction('1/2')*HTT
-Hbar += Fraction('1/6')*HTTT
-
-S = bra*Hbar
-out = apply_wick(S)
-out.resolve()
-final = AExpression(Ex=out)
-print(final)
-print(final._print_einsum())
+    print('\n ----------- T-2 term -----------\n')
+    bra = braE2("occ", "vir", "occ", "vir")
+    HTTT = commute(HTT, T)
+    HTTTT = commute(HTTT, T)
+    Hbar = H + HT + Fraction('1/2')*HTT
+    Hbar += Fraction('1/6')*HTTT + Fraction('1/24')*HTTTT
 
 
-print('\n ----------- p1E1 term -----------\n')
-bra = braP1E1('nm','occ', 'vir') 
-Hbar = H + HT + Fraction('1/2')*HTT
-Hbar += Fraction('1/6')*HTTT
+    S = bra*Hbar
+    out = apply_wick(S)
+    out.resolve()
+    final = AExpression(Ex=out)
+    print(final)
+    print(final._print_einsum('T2'))
 
-S = bra*Hbar
-out = apply_wick(S)
-out.resolve()
-final = AExpression(Ex=out)
-print(final)
-print(final._print_einsum())
+    print('\n ----------- p1 term -----------\n')
+    bra = braP1('nm') 
+    Hbar = H + HT + Fraction('1/2')*HTT
+    Hbar += Fraction('1/6')*HTTT
 
-sys.exit()
+    S = bra*Hbar
+    out = apply_wick(S)
+    out.resolve()
+    final = AExpression(Ex=out)
+    print(final)
+    print(final._print_einsum('S1'))
 
-#======================================================================
-#   EOM_CCSD for polariton
-#======================================================================
+    print('\n ----------- p1E1 term -----------\n')
+    bra = braP1E1('nm','occ', 'vir') 
+    Hbar = H + HT + Fraction('1/2')*HTT
+    Hbar += Fraction('1/6')*HTTT
+
+    S = bra*Hbar
+    out = apply_wick(S)
+    out.resolve()
+    final = AExpression(Ex=out)
+    print(final)
+    print(final._print_einsum('U11'))
+
+
 from wick.convenience import one_e, two_e, E1, E2, braE1, E1, E2, commute
-print(' \n EOM -CCSD equations\n')
+if get_EOMCCSD:
+    #======================================================================
+    #   EOM_CCSD for polariton
+    #======================================================================
+    print(' \n EOM-CCSD equations\n')
+    
+    print('\n CCSD Hamiltonian=\n')
+    print(H)
 
-H1 = one_e("f", ["occ", "vir"], norder=True)
-H2 = two_e("I", ["occ", "vir"], norder=True)
-H = H1 + H2
+    T1 = E1("T1", ["occ"], ["vir"])
+    T2 = E2("T2", ["occ"], ["vir"])
 
-bra = braE1("occ", "vir")
-T1 = E1("t", ["occ"], ["vir"])
-T2 = E2("t", ["occ"], ["vir"])
+    S1 = P1("S1", ["nm"])
+    S2 = P2("s2", ["nm"])
+    S3 = P3("s3", ["nm"])
+    print('S1=', S1,'\n')
+    print('S2=', S2,'\n')
+    print('S3=', S3,'\n')
 
-R1 = E1("r", ["occ"], ["vir"])
-R2 = E2("r", ["occ"], ["vir"])
+    # coupled fermion-boson excitation
+    U11 = EPS1("U11", ["nm"], ["occ"], ["vir"])
 
-T = T1 + T2
-R = R1 + R2
+    RS = E1("RS", ["occ"], ["vir"])
+    RD = E2("RD", ["occ"], ["vir"])
+    R1 = P1("R1", ["nm"])
+    R11 = EPS1("R11", ["nm"], ["occ"], ["vir"])
 
-HT = commute(H, T)
-HTT = commute(HT, T)
-HTTT = commute(HTT, T)
-HTTTT = commute(HTTT, T)
-Hbar = H + HT + Fraction('1/2')*HTT
+    T = T1 + T2 + S1 + U11
+    R = RS + RD + R1 + R11
 
-S0 = Hbar
-E0 = apply_wick(S0)
-E0.resolve()
-final = AExpression(Ex=E0)
-print(final)
-print(final._print_einsum())
+    HT = commute(H, T)
+    HTT = commute(HT, T)
+    HTTT = commute(HTT, T)
+    HTTTT = commute(HTTT, T)
+    Hbar = H + HT + Fraction('1/2')*HTT
 
-Hbar += Fraction('1/6')*HTTT + Fraction('1/24')*HTTTT
+    S0 = Hbar
+    E0 = apply_wick(S0)
+    E0.resolve()
 
-#   EOM - sigS part
-print('\nEOM-CCSD sigmaS part\n')
+    final = AExpression(Ex=E0)
 
+    Hbar += Fraction('1/6')*HTTT + Fraction('1/24')*HTTTT
+
+    # =================  EOM - sigS part =================
+    print('\nEOM-CCSD sigS part\n')
+    bra = braE1("occ", "vir")
+
+    S = bra*(Hbar - E0)*R
+
+    out = apply_wick(S)
+    out.resolve()
+    final = AExpression(Ex=out)
+    print(final)
+    print(final._print_einsum('SigS'))
+
+    # =================  EOM - sigD part =================
+    print('\nEOM-CCSD sigD part\n')
+    bra = braE2("occ", "vir", "occ", "vir")
+
+    S = bra*(Hbar - E0)*R
+
+    out = apply_wick(S)
+    out.resolve()
+    final = AExpression(Ex=out)
+    print(final)
+    print('\n einsum format=\n')
+    print(final._print_einsum('SigD'))
 
